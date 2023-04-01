a970afe5c5c29d244f162b016c3c8bb8a3ab596d
zzemlyanaya <zem.jane@gmail.com>
2023-04-01 14:50:31 +0500

    portscan

--- uni/internetProtocols_4sem/portscan.py
+++ uni/internetProtocols_4sem/portscan.py
@@ -1,70 +1,144 @@
 from argparse import ArgumentParser
-from typing import List
+from dataclasses import dataclass
 import socket
+import time
+import binascii
+import re
 from queue import Queue
-from threading import Thread, Lock
+from queue import Empty
+from threading import Thread, RLock
+
+protocol_names = {socket.SOCK_STREAM: 'TCP', socket.SOCK_DGRAM: 'UDP'}
 
 
-class Scanner:
-    def __init__(self, host_name: str, is_tcp: bool, ports_range: List[int]):
-        try:
-            self.host = socket.gethostbyname(host_name)
-        except socket.gaierror:
-            raise Exception('Unknown host\'s name')
-        if not self.check_ports_range(ports_range):
-            raise Exception('Wrong ports range')
-        self.is_tcp = is_tcp
-        self.print_lock = Lock()
-        self.ports = Queue()
-        for port in range(ports_range[0], ports_range[1] + 1):
-            self.ports.put(port)
+@dataclass
+class ScannerArgs:
+    host: str
+    scan_tcp: bool
+    scan_udp: bool
+    port_range: range
 
-    @staticmethod
-    def check_ports_range(ports_range: List[int]) -> bool:
-        return (-1 < ports_range[0] <= ports_range[1] < 2 ** 16
-                and len(ports_range) == 2)
 
-    @staticmethod
-    def get_app_protocol(port: int, trans_protocol: str) -> str:
-        try:
-            return socket.getservbyport(port, trans_protocol).upper()
-        except OSError:
-            return ''
+class HideousScanner:
+    def __init__(self, args: ScannerArgs, threads=1000, timeout=3):
+        self.ip = socket.gethostbyname(args.host)
+        self.ports = args.port_range
+        self.protocols = []
+        if args.scan_tcp:
+            self.protocols.append(socket.SOCK_STREAM)
+        if args.scan_udp:
+            self.protocols.append(socket.SOCK_DGRAM)
 
-    def scan_ports(self):
+        self.lock = RLock()
+        self.threads = threads
+
+        self.q = Queue(maxsize=self.threads * 3)
+        self.scan_data = ((self.ip, port, protocol) for port in self.ports for protocol in self.protocols)
+        self.timeout = timeout
+        self.scan_results = []
+
+    def fill_queue(self):
         while True:
-            port = self.ports.get()
-            self.scan_port(port)
-            self.ports.task_done()
+            if not self.q.full():
+                try:
+                    self.q.put(next(self.scan_data))
+                except StopIteration:
+                    break
+            else:
+                time.sleep(0.01)
 
-    def scan_port(self, port: int):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    def worker(self):
+        while True:
             try:
-                sock.settimeout(1)
-                r = sock.connect_ex((self.host, port))
-                if r == 0:
-                    service = self.get_app_protocol(port, 'tcp')
-                    with self.print_lock:
-                        print(f'TCP {port} {service}')
-            except (socket.timeout, ConnectionRefusedError):
-                pass
+                data = self.q.get()
+                if data[2] == socket.SOCK_STREAM:
+                    self.ping_tcp(*data)
+                else:
+                    self.ping_udp(*data)
+            except Empty:
+                return
+            finally:
+                self.q.task_done()
+
+    def ping_tcp(self, ip, port, protocol):
+        with socket.socket(socket.AF_INET, protocol) as sock:
+            sock.settimeout(self.timeout)
+            status = sock.connect_ex((ip, port))
+            if status == 0:
+                try:
+                    service = socket.getservbyport(port)
+                except:
+                    service = ''
+                finally:
+                    self.print_result(protocol, port, service)
+                    self.scan_results.append((ip, port, protocol))
+
+    def ping_udp(self, ip, port, protocol):
+        with socket.socket(socket.AF_INET, protocol) as sock:
+            sock.settimeout(self.timeout)
+            try:
+                sock.sendto(b'ping', (ip, port))
+                data, _ = sock.recvfrom(1024)
+                # ICMP (3,3) response = closed
+                if data.startswith(bytes([3, 3])):
+                    return
+            except socket.timeout:
+                try:
+                    service = socket.getservbyport(port)
+                except:
+                    service = ''
+                finally:
+                    self.print_result(protocol, port, service)
+                    self.scan_results.append((ip, port, protocol))
+            except socket.error:
+                return
+
+    def print_result(self, protocol, port, service):
+        with self.lock:
+            print('{} {} {}'.format(protocol_names[protocol], port, service))
 
     def run(self):
-        if self.is_tcp:
-            for _ in range(512):
-                Thread(target=self.scan_ports, daemon=True).start()
-            self.ports.join()
+        queue_thread = Thread(target=self.fill_queue)
+        queue_thread.daemon = True
+        queue_thread.start()
+
+        self.scan_results = []
+        for i in range(self.threads):
+            t = Thread(target=self.worker)
+            t.daemon = True
+            t.start()
+
+        self.q.join()
+        return self.scan_results
+
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('host', type=str, default='localhost')
+    parser.add_argument('-t', action='store_true', dest='scan_tcp', default=False)
+    parser.add_argument('-u', action='store_true', dest='scan_udp', default=False)
+    parser.add_argument('-p', '--ports', nargs='+', type=int, default=[0, 1023], dest='port_range')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('host_name', help='Host for scanning')
-    parser.add_argument('-t', action='store_true', dest='is_tcp')
-    parser.add_argument('-p', '--ports', nargs='+', type=int, default=[0, 80], dest='ports_range')
-
-    args = parser.parse_args().__dict__
     try:
-        scanner = Scanner(**args)
+        args = get_args()
+        if args.port_range[0] > args.port_range[1] or args.port_range[1] > 65535:
+            print('Incorrect ports')
+            exit(1)
+
+        if args.scan_udp and args.port_range[0] == 0:
+            args.port_range[0] += 1
+
+        ports = range(args.port_range[0], args.port_range[1] + 1)
+        scan_args = ScannerArgs(args.host, args.scan_tcp, args.scan_udp, ports)
+        scanner = HideousScanner(scan_args)
         scanner.run()
-    except Exception as e:
-        print(e)
+    except PermissionError:
+        print('Not enough rights. Try sudo or run as admin')
+        exit(1)
+
+# 45.33.32.156
+# ya.ru
+# github.com
